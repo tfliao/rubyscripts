135a308a1f6ab1bd050ccfbb1032f56bc073dd43
tfliao <tingfuliao@gmail.com>
2015-05-18 21:57:45 +0800

    add template for ruby cli code

---
+++ clibase.rb
@@ -0,0 +1,42 @@
+#! /usr/bin/ruby
+require 'optparse'
+require 'ostruct'
+
+def parse()
+	# init
+	options = OpenStruct.new
+	options.verbose = false
+	options.list = []
+	options.value = 0
+	options.remains = []
+
+	parser = OptionParser.new do |opts|
+		# banner for help message
+		opts.banner = "Usage: name.rb [options] args ... "
+
+		# register each option
+		opts.on("-h", "--help", "show this help message") do
+			puts opts
+			exit
+		end
+
+		## switch on/off
+		opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+			options.verbose = v
+		end
+		## load value
+		opts.on("-x=X", "--xvalue=X", "accept value") do |x|
+			options.value = x.to_i
+		end
+		## list type
+		opts.on("-l x, ...", "--list x, ...", Array, "accept list") do |l|
+			options.list.concat(l)
+		end
+	end
+	options.remains = parser.parse!
+
+	options
+end
+
+parse()
+
