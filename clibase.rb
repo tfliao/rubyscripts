fa5c72e6fc826a6fa9dcf14b4d01944a60abfbdf
tfliao <tfliao@synology.com>
2015-10-08 10:28:34 +0800

    classify clibase

--- clibase.rb
+++ clibase.rb
@@ -2,50 +2,62 @@
 require 'optparse'
 require 'ostruct'
 
-def parse()
-	# init
-	basename = File.basename(__FILE__, ".rb")
+class CliBase
+	@options = NIL
 
-	options = OpenStruct.new
-	options.verbose = false
-	options.list = []
-	options.value = 0
-	options.remains = []
-
-	parser = OptionParser.new do |opts|
-		# banner for help message
-		opts.banner =  "Usage: #{basename} [options] args ... "
-		opts.separator "       <description of program here>"
-		opts.separator ""
-
-		# common options
-		opts.on_tail("-h", "--help", "show this help message") do
-			puts opts
-			exit
-		end
-		opts.on_tail("--version", "show version") do
-			puts "#{basename} 1.0.0"
-			exit
-		end
-
-		# specific options
-		## switch on/off
-		opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
-			options.verbose = v
-		end
-		## load value
-		opts.on("-x=X", "--xvalue=X", "accept value") do |x|
-			options.value = x.to_i
-		end
-		## list type
-		opts.on("-l x, ...", "--list x, ...", Array, "accept list") do |l|
-			options.list.concat(l)
-		end
+	def _parse_init()
+		@options = OpenStruct.new
+		@options.verbose = false
+		@options.list = []
+		@options.value = 0
+		@options.remains = []
 	end
-	options.remains = parser.parse!
 
-	options
+	def parse(argv)
+		# init
+		basename = File.basename(__FILE__, ".rb")
+
+		_parse_init()
+
+		parser = OptionParser.new do |opts|
+			# banner for help message
+			opts.banner =  "Usage: #{basename} [options] args ... "
+			opts.separator "       <description of program here>"
+			opts.separator ""
+
+			# common options
+			opts.on_tail("-h", "--help", "show this help message") do
+				puts opts
+				exit
+			end
+			opts.on_tail("--version", "show version") do
+				puts "#{basename} 1.0.0"
+				exit
+			end
+
+			# specific options
+			## switch on/off
+			opts.on("-v", "--[no-]verbose", "run verbosely") do |v|
+				@options.verbose = v
+			end
+			## load value
+			opts.on("-x=X", "--xvalue=X", "accept value") do |x|
+				@options.value = x.to_i
+			end
+			## list type
+			opts.on("-l x, ...", "--list x, ...", Array, "accept list") do |l|
+				@options.list.concat(l)
+			end
+		end
+		@options.remains = parser.parse(argv)
+
+		@options
+	end
+	def run()
+		puts parse(ARGV)
+	end
 end
 
-parse()
-
+if __FILE__ == $0 then
+	CliBase.new.run
+end
